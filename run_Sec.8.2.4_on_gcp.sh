a95b3afc2800a5e1c090823c327116aaefb63189
hi32ce <hi32ce_2@hotmail.com>
2019-10-05 01:48:32 +0900

    Imp Sec.8.2.3

---
+++ run_Sec.8.2.4_on_gcp.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mvn compile exec:java -Dexec.mainClass=com.google.cloud.datascienceongcp.flights.CreateTrainingDataset_8_2_3 -Dexec.args="--project=ds-gcp-252814 --stagingLocation=gs://dsongcp_20190914/staging/ --input=gs://dsongcp_20190914/flights/chapter8/small.csv --output=gs://dsongcp_20190914/flights/chapter8/output/ --runner=DataflowRunner"
--- src/main/java/com/google/cloud/datascienceongcp/flights/CreateTrainingDataset_8_2_3.java
+++ src/main/java/com/google/cloud/datascienceongcp/flights/CreateTrainingDataset_8_2_3.java
@@ -18,16 +18,15 @@
 package com.google.cloud.datascienceongcp.flights;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.transforms.SimpleFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
 
 /**
  * A starter example for writing Beam programs.
@@ -41,25 +40,35 @@ import java.util.Arrays;
  *
  * <p>To run this starter example using managed resource in Google Cloud
  * Platform, you should specify the following command-line options:
- *   --project=<YOUR_PROJECT_ID>
- *   --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE>
- *   --runner=DataflowRunner
+ * --project=<YOUR_PROJECT_ID>
+ * --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE>
+ * --runner=DataflowRunner
  */
-public class CreateTrainingDataset {
-  private static final Logger LOG = LoggerFactory.getLogger(CreateTrainingDataset.class);
+public class CreateTrainingDataset_8_2_3 {
+  private static final Logger LOG = LoggerFactory.getLogger(CreateTrainingDataset_8_2_3.class);
+
+  public static interface MyOptions extends PipelineOptions {
+    @Description("Path of the file to read from")
+    @Default.String("./small.csv")
+    String getInput();
+
+    void setInput(String s);
+
+    @Description("Path of the output to read from")
+    @Default.String("./output/")
+    String getOutput();
+
+    void setOutput(String s);
+  }
 
   public static void main(String[] args) {
-    String[] events = {
-        "2015-09-20,AA,19805,AA,1572,13303,1330303,32467,MIA,12889,1288903,32211,LAS,2015-09-20T22:50:00,2015-09-20T04:03:00,313.00,19.00,2015-09-20T04:22:00,,,2015-09-21T04:08:00,,,0.00,,,2174.00,25.79527778,-80.29000000,-14400.0,36.08000000,-115.15222222,-25200.0,wheelsoff,2015-09-20T04:22:00",
-        "2015-09-20,AA,19805,AA,2495,11298,1129804,30194,DFW,12892,1289203,32575,LAX,2015-09-21T01:25:00,2015-09-20T06:04:00,279.00,15.00,2015-09-20T06:19:00,,,2015-09-21T04:55:00,,,0.00,,,1235.00,32.89722222,-97.03777778,-18000.0,33.94250000,-118.40805556,-25200.0,wheelsoff,2015-09-20T06:19:00",
-        "2015-09-20,AA,19805,AA,2342,11292,1129202,30325,DEN,13303,1330303,32467,MIA,2015-09-21T05:59:00,2015-09-20T06:33:00,34.00,14.00,2015-09-20T06:47:00,,,2015-09-20T09:47:00,,,0.00,,,1709.00,39.86166667,-104.67305556,-21600.0,25.79527778,-80.29000000,-14400.0,wheelsoff,2015-09-20T06:47:00"
-    };
+    MyOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+        .as(MyOptions.class);
+    Pipeline p = Pipeline.create(options);
 
-    Pipeline p = Pipeline.create(
-        PipelineOptionsFactory.fromArgs(args).withValidation().create());
-
-    p.apply(Create.of(Arrays.asList(events)))
-        .apply(ParDo.of(new DoFn<String, String>() {
+    p
+        .apply("ReadLines", TextIO.read().from(options.getInput()))
+        .apply("FilterMIA", ParDo.of(new DoFn<String, String>() {
           @ProcessElement
           public void processElement(ProcessContext c) throws Exception {
             String input = c.element();
@@ -68,12 +77,8 @@ public class CreateTrainingDataset {
             }
           }
         }))
-        .apply(ParDo.of(new DoFn<String, Void>() {
-          @ProcessElement
-          public void processElement(ProcessContext c) throws Exception {
-            LOG.info(c.element());
-          }
-        }));
+        .apply("WriteFlights", TextIO.write().to(options.getOutput() + "flights2")
+            .withSuffix(".txt").withoutSharding());
 
     p.run();
   }
